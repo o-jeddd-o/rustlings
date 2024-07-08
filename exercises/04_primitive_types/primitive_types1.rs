f090c5de7aa42abc548d82227087fe819f0810d7
o-jeddd-o <jedidiahinyang@gmail.com>
2024-07-08 11:44:57 +0100

    cleared the priimitive types exercises

--- exercises/04_primitive_types/primitive_types1.rs
+++ exercises/04_primitive_types/primitive_types1.rs
@@ -1,14 +1,14 @@
 // Booleans (`bool`)
 
 fn main() {
-    let is_morning = true;
+    let is_morning = false;
     if is_morning {
         println!("Good morning!");
     }
 
     // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
     // The value of the variable should be the negation (opposite) of `is_morning`.
-    // let …
+    let is_evening = true; // can't be morning and evening at the same time.
     if is_evening {
         println!("Good evening!");
     }
